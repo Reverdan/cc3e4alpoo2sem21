b918018f66021c97ac42a05be2dac93974ea5975
Reverdan Almeida Sparinger <reverdan@gmail.com>
2021-10-05 20:37:21 -0300

    APS

--- Estacao/APSMeteorologica/src/Modelo/Serial.java
+++ Estacao/APSMeteorologica/src/Modelo/Serial.java
@@ -1,10 +1,13 @@
 package Modelo;
 
+import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.Enumeration;
 
 public class Serial implements SerialPortEventListener
 {
@@ -22,23 +25,89 @@ public class Serial implements SerialPortEventListener
 
     public boolean iniciaSerial()
     {
-
+        boolean status = false;
+        try
+        {
+            CommPortIdentifier portId = null;
+            Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+            while(portId == null && portEnum.hasMoreElements())
+            {
+                CommPortIdentifier currentPortId = (CommPortIdentifier)portEnum.nextElement();
+                if (currentPortId.getName().equals(serialPortName))
+                {
+                    serialPort = (SerialPort) currentPortId.open(appName, TIME_OUT);
+                    portId = currentPortId;
+                    break;
+                }
+            }
+            if (portId == null || serialPort == null)
+                return false;
+            serialPort.setSerialPortParams(DATA_RATE, serialPort.DATABITS_8, 
+                    serialPort.STOPBITS_1, serialPort.PARITY_NONE);
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+            status = true;
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            status = false;
+        }
+        return status;
     }
 
-    public void sendData()
+    public void sendData(String data)
     {
-
+        try
+        {
+            output = serialPort.getOutputStream();
+            output.write(data.getBytes());
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.toString());
+        }
     }
 
     public synchronized void close()
     {
-
+        serialPort.removeEventListener();
+        serialPort.close();
     }
 
     @Override
     public void serialEvent(SerialPortEvent spe)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try
+        {
+            switch(spe.getEventType())
+            {
+                case SerialPortEvent.DATA_AVAILABLE:
+                    if (input == null)
+                    {
+                        input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+                    }
+                    if (input.ready())
+                    {
+                        protocolo.setLeituraComando(input.readLine());
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static int getDATA_RATE()
